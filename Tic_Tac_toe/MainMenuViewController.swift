d9b48109c66cd908f117ce6074e497b5c9cd19a3
Adrian Kubała <adrian.kubala@gmail.com>
2016-11-25 14:37:57 +0100

    Add view controllers' classes

---
+++ Tic_Tac_toe/MainMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainMenuViewController.swift
+//  Tic_Tac_toe
+//
+//  Created by Adrian Kubała on 25.11.2016.
+//  Copyright © 2016 Adrian Kubała. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
