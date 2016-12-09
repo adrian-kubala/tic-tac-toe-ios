43eb3950ab29861fafcae6341724e2ac58f9be4a
Adrian Kubała <adrian.kubala@gmail.com>
2016-12-09 21:24:59 +0100

    Create view class

---
+++ Tic_Tac_toe/FieldView.swift
@@ -0,0 +1,13 @@
+//
+//  FieldView.swift
+//  Tic_Tac_toe
+//
+//  Created by Adrian Kubała on 09.12.2016.
+//  Copyright © 2016 Adrian Kubała. All rights reserved.
+//
+
+import UIKit
+
+class FieldView: UIButton {
+  var isClicked = false
+}
