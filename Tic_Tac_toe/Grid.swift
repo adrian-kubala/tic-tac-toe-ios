d9c588fabde1c6749950e45cd2e594fa34f0f33e
Adrian Kubała <adrian.kubala@gmail.com>
2016-12-09 23:45:24 +0100

    Implement determinating the game's end

--- Tic_Tac_toe/Grid.swift
+++ Tic_Tac_toe/Grid.swift
@@ -11,5 +11,39 @@ import Foundation
 class Grid {
   let fields: [Field] = []
   var isFull = false
+  var circleIsWinner: Bool?
   
+  func hasWinner() -> Bool {
+    if allFieldsAreClicked(fields[0], fields[1], fields[2])
+    || allFieldsAreClicked(fields[3], fields[4], fields[5])
+    || allFieldsAreClicked(fields[6], fields[7], fields[8])
+      
+    || allFieldsAreClicked(fields[0], fields[3], fields[6])
+    || allFieldsAreClicked(fields[1], fields[4], fields[7])
+    || allFieldsAreClicked(fields[2], fields[5], fields[8])
+    
+    || allFieldsAreClicked(fields[0], fields[4], fields[8])
+    || allFieldsAreClicked(fields[2], fields[4], fields[6]) {
+      return true
+    }
+    
+    return false
+  }
+  
+  fileprivate func allFieldsAreClicked(_ fields: Field...) -> Bool {
+    let firstFieldIsCircle = fields[0].isCircle
+    
+    for field in fields {
+      if field.isClicked && field.isCircle && firstFieldIsCircle! {
+        continue
+      } else if field.isClicked && !field.isCircle && !firstFieldIsCircle! {
+        continue
+      } else {
+        return false
+      }
+    }
+    
+    circleIsWinner = firstFieldIsCircle! ? true : false
+    return true
+  }
 }
